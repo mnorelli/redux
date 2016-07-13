be3f3846cf3a5ae215f7efcc73620831bf220072
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-07-12 19:29:18 -0700

    cleans starter code

--- starter-code/app/index.jsx
+++ starter-code/app/index.jsx
@@ -2,20 +2,4 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/app'
-import store from './reducers/rootReducer'
-
-// update the DOM with the new state
-const renderView = () => {
-  ReactDOM.render(
-    <App/>,
-    document.getElementById("app")
-  )
-}
-
-// page initialization
-renderView()
-
-// re-render every time the store is updated
-store.subscribe(renderView)
 
--- starter-code/app/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-import likes from "./likes"
-import foods from "./foods"
-
-const app = combineReducers({likes, foods})
-
-const store = createStore(app)
-
-export default store
